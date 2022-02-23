b55e0d8ab3662f694c039e3868fdfd2d5606a556
Anthony-Carlisi <anthonycarlisi95@gmail.com>
2022-02-23 09:50:22 -0500

    commit

--- AUTOUPDATE.sh
+++ AUTOUPDATE.sh
@@ -2,4 +2,4 @@
 
 cd /var/opt/Code-Clean-up
 
-git pull https://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@github.com/Anthony-Carlisi/SCRUB-TOOL.git master
+git pull https://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@github.com/Anthony-Carlisi/Code-Clean-up.git master
