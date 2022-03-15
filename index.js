fd208b48701b8808cf83ffcf5fbfe681ef51abcb
Anthony-Carlisi <anthonycarlisi95@gmail.com>
2022-03-15 13:48:42 -0400

    Revert "test"
    This reverts commit a623a716cd0298d460e47a46fe69d7c6dea78e57.

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ app.get('/api/create', function (req, res) {
           req.query.Assignees = userInfo[0].fields.Name
           if (!merchantRecords.length) {
             airtableHelper
-              .airtableSearch(
+              .airtableSearch22(
                 req.query['Business Phone'],
                 '{Mobile Phone Formatted}',
                 'Inbound Leads'
