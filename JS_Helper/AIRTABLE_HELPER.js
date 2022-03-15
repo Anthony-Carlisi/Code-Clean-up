162424a0da55acb61edf2a69b6f09410907810bf
Anthony-Carlisi <anthonycarlisi95@gmail.com>
2022-03-15 14:19:59 -0400

    Revert "commit"
    This reverts commit e33fcf341f214bd5d104890ab3c7d87a1dd1039f.

--- JS_Helper/AIRTABLE_HELPER.js
+++ JS_Helper/AIRTABLE_HELPER.js
@@ -1,96 +1,98 @@
-const moment = require('moment')
-const rico = require('./RICOCHET_HELPER')
+const moment = require('moment');
+const rico = require('./RICOCHET_HELPER');
 var Airtable = require('airtable'),
-  base = new Airtable({ apiKey: 'keyZ0WMDk0n8pJ7yl' }).base('appumwVbyIXggVLmr')
+  base = new Airtable({ apiKey: 'keyZ0WMDk0n8pJ7yl' }).base(
+    'appumwVbyIXggVLmr'
+  );
 
 const airtableSearch = async (searchField, fieldToSearch, tableToSearch) => {
   const records = await base(tableToSearch)
     .select({
       filterByFormula: `${fieldToSearch} = "${searchField}"`,
     })
-    .all()
+    .all();
   if (typeof records[0] !== 'undefined') {
-    return records[0]
+    return records[0];
   } else {
-    return undefined
+    return undefined;
   }
-}
+};
 
 const airtableSearch2 = async (searchField, fieldToSearch, tableToSearch) => {
   const records = await base(tableToSearch)
     .select({
-      filterByFormula: `AND(SEARCH(${fieldToSearch}, '${searchField}' & ""), DATETIME_DIFF({Status Change Date (DUPS)}, DATEADD(TODAY(),-40,'days'), 'days') > 0`,
+      filterByFormula: `AND(SEARCH(${fieldToSearch}, '${searchField}' & ""), DATETIME_DIFF({Status Change Date (DUPS)}, DATEADD(TODAY(),-40,'days'), 'days') = 0`,
     })
-    .all()
-  return records
-}
+    .all();
+  return records;
+};
 
 const airtableSearch3 = async (searchField, fieldToSearch, tableToSearch) => {
   const records = await base(tableToSearch)
     .select({
       filterByFormula: `AND(SEARCH(${fieldToSearch}, '${searchField}' & ""), {Lead Type (Vehicle)} = "SMS We Process")`,
     })
-    .all()
+    .all();
   if (typeof records[0] !== 'undefined') {
-    return records[0]
+    return records[0];
   } else {
-    return undefined
+    return undefined;
   }
-}
+};
 
 const airtableSearch4 = async () => {
   const records = await base('Merchant Records')
     .select({
       filterByFormula: `IF({Sub Status},'true')`,
     })
-    .all()
-  return records
-}
+    .all();
+  return records;
+};
 
 async function airtableSubstatus() {
   try {
-    let results = await airtableSearch4()
+    let results = await airtableSearch4();
     results.forEach((jsdata) => {
       if (jsdata.fields['Sub Status'] !== 'Call Back') {
         var updateRecord = {
           fields: { Status: jsdata.fields['Sub Status'], 'Sub Status': null },
-        }
-        airtableUpdate(updateRecord.fields, jsdata.id, 'Merchant Records')
+        };
+        airtableUpdate(updateRecord.fields, jsdata.id, 'Merchant Records');
       } else if (
         jsdata.fields['Sub Status Change Date'] <
         jsdata.fields['Status Change Date']
       ) {
         var updateRecord = {
           fields: { 'Sub Status': null },
-        }
-        airtableUpdate(updateRecord.fields, jsdata.id, 'Merchant Records')
+        };
+        airtableUpdate(updateRecord.fields, jsdata.id, 'Merchant Records');
       }
-    })
+    });
     //return results;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 function airtableCreate(data, tableToCreate) {
   base(tableToCreate).create([data], { typecast: true }, (err, record) => {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      return;
     }
-    return record[0]
-  })
+    return record[0];
+  });
 }
 
 function airtableUpdate(data, record_id, tableToUpdate) {
   base(tableToUpdate).update(record_id, data, (err, record) => {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      return;
     }
-    console.log(record.getId())
-    return record
-  })
+    console.log(record.getId());
+    return record;
+  });
 }
 
 const airtableSearch5 = async () => {
@@ -99,12 +101,12 @@ const airtableSearch5 = async () => {
       .select({
         filterByFormula: `DATETIME_DIFF({Status Change Date (DUPS)}, DATEADD(TODAY(),-90,'days'), 'days') = 0`,
       })
-      .all()
-    return records
+      .all();
+    return records;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 // var test = moment(Date.now()).format('YYYY-MM-DD');
 // console.log(test);
@@ -121,4 +123,4 @@ module.exports = {
   airtableSubstatus,
   airtableCreate,
   airtableUpdate,
-}
+};
