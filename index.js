a18d7f4775fb3b2e878a7488d29193f5728b8b9b
Anthony Carlisi <anthonycarlisi95@gmail.com>
2022-04-14 11:17:04 -0400

    comiit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express'),
   rico = require('./JS_Helper/RICOCHET_HELPER')
 //Scripts
 
-//Comment test test test
+//Comment
 const app = express()
 
 app.use(express.json({ extended: false }))
