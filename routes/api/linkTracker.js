f43f06068f2378d106f9e8a5b7b6350e9fc13306
Anthony-Carlisi <anthonycarlisi95@gmail.com>
2022-04-15 15:03:03 -0400

    commit

--- routes/api/linkTracker.js
+++ routes/api/linkTracker.js
@@ -6,13 +6,13 @@ const { google } = require('googleapis')
 const crypto = require('crypto')
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: '/config/keys.json', //the key file
+  keyFile: 'config/keys.json', //the key file
   //url to spreadsheets API
   scopes: 'https://www.googleapis.com/auth/spreadsheets',
 })
 
-// @route   Post api/test
-// @desc    test
+// @route   Post api/linkTracker
+// @desc    Create unique IDs for tracking per lead
 // @access  Public
 router.post('/', async (req, res) => {
   try {
@@ -30,23 +30,31 @@ router.post('/', async (req, res) => {
     } = req.body
     phone = phone.replace(/-/g, '')
 
-    const authClientObject = await auth.getClient()
+    //Dup Blocking
+    const dupCheck = await dupBlockerCheck([phone])
+    if (dupCheck?.length > 0) return res.send(`This Lead is a Dup Block`)
 
+    // Auth with Google API
+    const authClientObject = await auth.getClient()
+    // Declare the API
     const googleSheetsInstance = google.sheets({
       version: 'v4',
       auth: authClientObject,
     })
-
+    // What spreadsheet to edit
     const spreadsheetId = '1tVc8cYS_724wem4gr5bcDuNCTSRQXfAwa-6YsyStfvA'
 
+    // Reading from a spreadsheet
     const readData = await googleSheetsInstance.spreadsheets.values.get({
       auth, //auth object
       spreadsheetId, // spreadsheet id
       range: 'Leads!D:D', //range of cells to read from.
     })
 
+    // Array of all Emails within column
     const prevEmails = readData.data.values
 
+    // Checking to see if email already exists within the list
     let check = 0
     prevEmails.map((prevEmail) => {
       if (prevEmail[0] === email) check = check + 1
@@ -56,15 +64,18 @@ router.post('/', async (req, res) => {
     // Next Row
     const newRowNum = readData.data.values.length + 1
 
-    const linkId = crypto.randomUUID()
+    // Creates unique IDs per lead
+    const linkId = crypto.randomUUID({ disableEntropyCache: true })
+    // console.log(linkId)
+    // Link to track
+    const link = `https://api.straightlinesource.com/api/linkTracker/t/${linkId}`
 
-    const link = `https://api.straightlinesource.com/t/${linkId}`
-    //write data into the google sheets
+    // Write data into the google sheets
     await googleSheetsInstance.spreadsheets.values.append({
       auth, //auth object
       spreadsheetId, //spreadsheet id
       range: `Leads!A${newRowNum}:I${newRowNum}`, //sheet name and range of cells
-      valueInputOption: 'USER_ENTERED', // The information will be passed according to what the usere passes in as date, number or text
+      valueInputOption: 'USER_ENTERED', // The information will be passed according to what the user passes in as date, number or text
       resource: {
         values: [
           [
@@ -77,6 +88,7 @@ router.post('/', async (req, res) => {
             source,
             purchaseDate,
             leadSource,
+            0,
             linkId,
             link,
           ],
@@ -84,14 +96,51 @@ router.post('/', async (req, res) => {
       },
     })
 
-    //Dup Blocking
-    const dupCheck = await dupBlockerCheck([phone])
-    if (dupCheck?.length > 0) {
-      console.log(dupCheck[0][0]._table.name)
-      return res.send(`This Lead is a Dup Block`)
+    res.send(`New Lead Created`)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+
+router.get('/t/:id', async (req, res) => {
+  try {
+    console.log(req.params.id)
+    const trackerId = req.params.id
+
+    // Auth with Google API
+    const authClientObject = await auth.getClient()
+    // Declare the API
+    const googleSheetsInstance = google.sheets({
+      version: 'v4',
+      auth: authClientObject,
+    })
+    // What spreadsheet to edit
+    const spreadsheetId = '1tVc8cYS_724wem4gr5bcDuNCTSRQXfAwa-6YsyStfvA'
+
+    // Reading from a spreadsheet
+    const readData = await googleSheetsInstance.spreadsheets.values.batchGet({
+      auth, //auth object
+      spreadsheetId, // spreadsheet id
+      ranges: ['Leads!A:K'], //range of cells to read from.
+    })
+
+    const tableObj = () => {
+      var batchRowValues = readData.data['valueRanges'][0]['values']
+      var rows = []
+      for (var i = 1; i < batchRowValues.length; i++) {
+        var rowObject = {}
+        for (var j = 0; j < batchRowValues[i].length; j++) {
+          rowObject[batchRowValues[0][j]] = batchRowValues[i][j]
+        }
+        rows.push(rowObject)
+      }
+      return rows
     }
 
-    res.send(`New Lead Created`)
+    console.log(tableObj())
+
+    res.redirect('https://straightlinesource.com/')
   } catch (err) {
     console.error(err.message)
     res.status(500).send('Server Error')
--- routes/api/sms.js
+++ routes/api/sms.js
@@ -205,4 +205,145 @@ router.post('/origination', async (req, res) => {
   }
 })
 
+router.post('/shm', async (req, res) => {
+  try {
+    // Deconstruct object from Ricochet
+
+    let {
+      'Lead Source': leadSource,
+      Vendor: vendor,
+      first_name: firstName,
+      last_name: lastName,
+      full_name: fullName,
+      email,
+      phone,
+      tags,
+      company_name: companyName,
+      message: { type: messageType },
+      message: { body: messageBody },
+      address1: address,
+      state,
+      city,
+      location: { name: locationName },
+      'Campaign ID': campaignId,
+    } = req.body
+    // Remove first two characters from phone +1
+
+    if (filter.isProfane(messageBody)) return res.send(`Profane language`)
+
+    const leadTypeId =
+      locationName === 'We Process'
+        ? ['recYN3IouIGLcYnNm']
+        : ['recxpVfCHIqJCFTtG']
+    phone = phone.slice(2)
+
+    if (messageType === 3)
+      messageBody = messageBody.split('\n')[0].replace(/(\[.*?\])/g, '')
+
+    //inbound lead check
+    const inboundLeadCheck = await airtableHelper.airtableSearch(
+      'Inbound Leads',
+      `OR({Mobile Phone Formatted} = ${phone}, {Business Phone Formatted} = ${phone})`,
+      'Scrubbing Tool'
+    )
+
+    if (inboundLeadCheck?.length > 0) {
+      // Deconstruct lead to update
+      const {
+        'Lead Type (Vehicle)': leadType,
+        'Merchant Name': merchantName,
+        Email: email,
+        'Mobile Phone': mobilePhone,
+        'Business Phone': businessPhone,
+        'Customer Response': customerResponse,
+        'Agent Email': agentEmails,
+      } = inboundLeadCheck[0].fields
+
+      // Get Inbound Lead ID
+      const updateId = inboundLeadCheck[0].id
+      // Concat prev response with new response
+      const updateCustomerResponse = {
+        'Customer Response': customerResponse + '\r\n' + messageBody,
+      }
+
+      const leadTypeInfo = await airtableHelper.airtableSearch(
+        'Marketing Data',
+        `{recordId} = '${leadType}'`,
+        'Grid view'
+      )
+
+      const leadTypeName = leadTypeInfo[0].fields['Marketing Method']
+
+      //Promises created to create the emails to send out
+      const newSmsEmail = agentEmails.map(async (agentEmail) => {
+        const emailAgent = await emailNotification.sendNotification(
+          `${agentEmail}`,
+          `SMS UPDATE ${leadTypeName} ${merchantName} ${email} ${mobilePhone}`,
+          `
+          You have an Updated ${leadTypeName} in your Stacker 'Hot Leads' table.
+          Merchant Name: ${merchantName}
+          Merchant Mobile: ${mobilePhone}
+          Business Phone: ${businessPhone}
+          Merchant Email: ${email}
+          `
+        )
+        if (emailAgent) return emailAgent
+      })
+
+      //Sends emails to all reciptiants
+      await Promise.all(newSmsEmail)
+
+      // Update inbound lead with new response
+      const updatedLead = await airtableHelper.airtableUpdate(
+        'Inbound Leads',
+        updateId,
+        updateCustomerResponse
+      )
+
+      // ends statement on update
+      return updatedLead
+    }
+
+    const leadSourceInfo = await airtableHelper.airtableSearch(
+      'Lead Source',
+      `{Lead Source} = '${leadSource}'`,
+      'Grid view'
+    )
+
+    let leadSourceId
+    if (leadSourceInfo?.length > 0) leadSourceId = [leadSourceInfo[0].id]
+
+    const vendorInfo = await airtableHelper.airtableSearch(
+      'Vendor List',
+      `{Vendor} = '${vendor}'`,
+      'Grid view'
+    )
+
+    const vendorId = vendorInfo[0].id
+
+    //  Create Object to send to Airtable
+    const airtableLead = {
+      'Customer Response': messageBody,
+      Email: email,
+      'Merchant First Name': firstName,
+      'Merchant Last Name': lastName,
+      'Mobile Phone': phone,
+      'Company Name': companyName,
+      'Agent Status': 'New Lead',
+      'Processing Status': 'New Lead',
+      'Tag (Vendor)': [vendorId],
+      'Lead Source (iMerchant Lead Source)': leadSourceId,
+      'Lead Type (Vehicle)': leadTypeId,
+      campaignID: campaignId,
+    }
+
+    await airtableHelper.airtableCreate('Inbound Leads', airtableLead)
+
+    res.send(`New Lead Created`)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+
 module.exports = router
